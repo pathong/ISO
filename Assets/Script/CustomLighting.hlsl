e507318308b98c3aa3d9f6c20c29dfd8201c887f
paopeaw <paopeaw8246@gmail.com>
2022-12-27 20:34:08 +0700

    Create Toon shader v3

--- Assets/Script/CustomLighting.hlsl
+++ Assets/Script/CustomLighting.hlsl
@@ -21,26 +21,6 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 }
 
 
-/*void AdditionalLight_float(float3 WorldPos, int Index,out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten )*/
-/*{*/
-	/*Direction = normalize(float3(.5f,.5f.25f));*/
-	/*Color = float3(.0f,.0f,.0f);*/
-	/*DistanceAtten = .0f;*/
-	/*ShadowAtten = .0f;*/
-/*#ifndef SHADERGRAPH_PREVIEW*/
-	/*int pixelLightCount = GetAdditionalLightCount();*/
-	/*if(Index < pixelLightCount){*/
-		/*Light light = GetAdditionalLight(Index, WorldPos);*/
-
-
-		/*Direction = light.direction;*/
-		/*Color = light.color;*/
-		/*DistanceAtten = light.distanceAttenuation;*/
-		/*shadowAttenuation = light.shadowAttenuation;*/
-	/*}*/
-/*#endif*/
-/*}*/
-
 
 
 
